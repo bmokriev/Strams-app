32b14d301b5f0d3ae8d00c640aeeacd7c9fcf5af
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-10-04 21:38:36 +0300

    initializing gapi

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 export class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        client_id:
+          '424101099769-g4av8fb6gs23e8nethp56pnhs0u1da3s.apps.googleusercontent.com',
+        scope: 'email',
+      });
+    });
+  }
   render() {
     return <div>GoogleAuth</div>;
   }
