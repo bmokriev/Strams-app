056d86f5b2eaa63badde6aa233c06a6f25fa656d
bmokriev <66565502+bmokriev@users.noreply.github.com>
2021-10-05 10:55:06 +0300

    Check if user is Signed in or not

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 
 export class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        client_id:
-          '424101099769-g4av8fb6gs23e8nethp56pnhs0u1da3s.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          client_id:
+            '424101099769-g4av8fb6gs23e8nethp56pnhs0u1da3s.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Don't know</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed in</div>;
+    } else {
+      return <div>NOT Signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
